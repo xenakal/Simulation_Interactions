0a6bc344c955770e71a968cd8bff27e050df3660
xenakal <alexandros.xenakis@student.uclouvain.be>
2020-03-21 14:55:18 +0100

    minor changes

--- multi_agent/tools/memory.py
+++ multi_agent/tools/memory.py
@@ -49,8 +49,6 @@ class Memory:
             target_predictor.add_measurement(state)
             # update current position list
             new_position_estimation = target_predictor.get_current_position()
-            self.update_memory_agent(time_from_estimation, agent_id, agent_signature, target_id, target_signature,
-                                     new_position_estimation[0], new_position_estimation[1], target_size)
 
     def add_target_estimator(self, estimator):
         self.memory_all_agent.add_target_estimator(estimator)
@@ -137,13 +135,3 @@ class Memory:
             return None
         return target_predictor.get_current_position()
 
-    def update_memory_agent(self, time_from_estimation, agent_id, agent_signature, target_id, target_signature,
-                            target_xc, target_yc, target_size):
-        """
-        Assumes the predictor corresponding to target_id has new information (doesn't check).
-        Updates the memory_agent list for the target.
-        """
-        new_TargetEstimator = TargetEstimator(time_from_estimation, agent_id, agent_signature, target_id,
-                                              target_signature,
-                                              target_xc, target_yc, target_size)
-        self.memory_agent.add_TargetEstimator(new_TargetEstimator)
