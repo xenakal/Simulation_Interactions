8c7c52186a448de10a8523871b5183113b0a287c
xenakal <alexandros.xenakis@student.uclouvain.be>
2020-03-21 15:17:13 +0100

    gitignore again

--- run_multiple_test.py
+++ run_multiple_test.py
@@ -1,6 +1,6 @@
 from main import*
 
-map_to_test_name = ["My_new_map","My_new_map1"]
+map_to_test_name = ["My_new_map", "My_new_map1"]
 for name in map_to_test_name:
     print("processing "+str(name))
     App(name).main()
